0856588edc0b9c7d4ce7b4905d1093d558e303d8
Nick Frattaroli <Nafratt@gmail.com>
2019-10-29 22:27:53 -0400

    adding colins fixes

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -2,9 +2,14 @@
 var mysql = require('mysql');
 // require inquirer
 var inquirer = require('inquirer');
-const conf= require('./config.js')
+// require config
+const conf = require('./config.js')
 var connection = mysql.createConnection(conf)
-
+var items= null
+connection.connect(function (err){
+if (err) throw err
+init();
+});
 function init() {
   //prints the items for sale and info 
   connection.query('SELECT * FROM Products', function (err, res) {
@@ -14,77 +19,68 @@ function init() {
       console.log('--------------------------------------------------------------------------------------------------')
       if (err) throw err;
     }
+    items=res
+    makepurchse()
   })
 }
-
-inquirer.prompt([
-  {
-    type: "input",
-    name: "id",
-    message: "What is the ID of the product you would like to purchase?",
-    validate: function (value) {
-      if (isNaN(value) == false && parseInt(value) <= res.length && parseInt(value) > 0) {
-        return true;
-      }
-      else {
-        return false;
-      }
-    }
-  },
-
-  {
-    type: "input",
-    name: "amount",
-    message: "How much would you like to purchase?",
-    validate: function (value) {
-      if (isNaN(value)) {
-        return false;
-      } else {
-        return true;
-      }
-    }
-  }
-]).then(function (ans) {
-  var whatToBuy = (ans.id) - 1;
-  var howMuch = parseInt(ans.qty);
-  var Total = parseFloat(((res[whatToBuy].Price) * howMuch).toFixed(2));
-
-  //check if amount is available 
-  if (res[whatToBuy].stock_quantity >= howMuch) {
-    //after purchase, updates quantity in Products
-    connection.query("UPDATE Products SET ? WHERE ?", [
-      { StockQuantity: (res[whatToBuy].stock_quantity - howMuch) },
-      { ItemID: ans.id }
-    ], function (err, result) {
-      if (err) throw err;
-      console.log("Success! Your total is $" + Total.toFixed(2) +
-        ". it will be shipped soon ");
-    });
-    connection.query("SELECT * FROM Departments", function (err, deptRes) {
-      if (err) throw err;
-      var index;
-      for (var i = 0; i < deptRes.length; i++) {
-        if (deptRes[i].department_name === res[whatToBuy].department_name) {
-          index = i;
+function makepurchse() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "id",
+      message: "What is the ID of the product you would like to purchase?",
+      validate: function (value) {
+        if (isNaN(value) == false &&  parseInt(value) > 0) {
+          return true;
+        }
+        else {
+          return false;
         }
       }
-      //takes away selected amount from db
-      connection.query("UPDATE Departments SET ? WHERE ?", [
-        { TotalSales: deptRes[index].TotalSales + grandTotal },
-        { DepartmentName: res[whatToBuy].department_name }
-      ], function (err, deptRes) {
+    },
+
+    {
+      type: "input",
+      name: "amount",
+      message: "How much would you like to purchase?",
+      validate: function (value) {
+        if (isNaN(value)) {
+          return false;
+        } else {
+          return true;
+        }
+      }
+    }
+  ]).then(function (ans) {
+    var whatToBuy = items.find(function(it){return it.item_id==ans.id});
+    var howMuch = parseInt(ans.amount);
+    console.log(items);
+    var Total = parseFloat(whatToBuy.price * howMuch).toFixed(2);
+    
+
+    //check if amount is available 
+    if (whatToBuy.stock_quantity >= howMuch) {
+      //after purchase, updates quantity in Products
+      connection.query("UPDATE Products SET ? WHERE ?", [
+        { stock_quantity: (whatToBuy.stock_quantity - howMuch) },
+        { item_id: ans.id }
+      ], function (err, result) {
         if (err) throw err;
-        //console.log("Updated Dept Sales.");
+        console.log("Success! Your total is $" + Total +
+          ". it will be shipped soon ");
+          askagain();
       });
-    });
 
-  } else {
-    console.log("Sorry, not enough stock");
-  }
 
-  askagain();
-})
+    } else {
+      
+      console.log("not enough in stock");
+      askagain();
+    }
 
+
+  })
+}
 // }
 //ask if they are done
 function askagain() {
@@ -94,10 +90,9 @@ function askagain() {
     message: "Would you like to keep shopping?"
   }]).then(function (ans) {
     if (ans.reply) {
-      start();
+      init();
     } else {
       console.log("See you soon!");
     }
   });
 }
-init();
